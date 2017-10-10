7a95e9cebaadeed31390ed29d7ef4ce04699e49b
Nandathilaka <thilinachamikashtc@gmail.com>
2017-10-10 22:05:19 +0530

    Me thiyenne aluthen hadapu sampurna Project eka.Meke Employee wena wenama log unama yanna oni Interface tika hadala thiyenne.

--- ApekshaPMS/src/com/apekshapms/controller/SearchPatientController.java
+++ ApekshaPMS/src/com/apekshapms/controller/SearchPatientController.java
@@ -2,6 +2,8 @@ package com.apekshapms.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import javax.swing.text.TabableView;
 import java.awt.*;
@@ -9,6 +11,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SearchPatientController implements Controller{
+
+
+
 
     @FXML
     private Label lblSearchPatient;
--- ApekshaPMS/src/com/apekshapms/ui/UIName.java
+++ ApekshaPMS/src/com/apekshapms/ui/UIName.java
@@ -2,11 +2,24 @@ package com.apekshapms.ui;
 
 public enum UIName {
     DASHBOARD("/com/apekshapms/ui/view/Dashboard.fxml"),
+    REGISTER_DOCTOR_DASHBOARD("/com/apekshapms/ui/view/main/RegisterDoctorDashboard.fxml"),
+    CONSULTANT_DASHBOARD("/com/apekshapms/ui/view/main/ConsultantDashboard.fxml"),
+    LAB_ASSISTANT_DASHBOARD("/com/apekshapms/ui/view/main/LabAssistantDashboard.fxml"),
+    ADMIN_DASHBOARD("/com/apekshapms/ui/view/main/AdminDashboard.fxml"),
+
+
+    //Admin Dashborad
+    PATIENT_DASHBOARD("/com/apekshapms/ui/view/admin/PatientDashboard.fxml"),
+    EMPLOYEE_DASHBOARD("/com/apekshapms/ui/view/admin/EmployeeDashboard.fxml"),
+    REPORT_DASHBOARD("/com/apekshapms/ui/view/admin/ReportDashboard.fxml"),
+    WARD_DASHBOARD("/com/apekshapms/ui/view/admin/WardDashboard.fxml"),
+    TREATMENT_DASHBOARD("/com/apekshapms/ui/view/admin/TreatmentDashboard.fxml"),
 
     NEW_PATIENT("/com/apekshapms/ui/view/NewPatient.fxml"),
     PATIENT_HISTORY("/com/apekshapms/ui/view/PatientHistory.fxml"),
     ASSIGNING("/com/apekshapms/ui/view/Assigning.fxml"),
     SOME_LOGIN("/com/apekshapms/ui/view/SomeLogin.fxml"),
+    LOGIN("/com/apekshapms/ui/view/login.fxml"),
 
     SEARCH_PATIENT("/com/apekshapms/ui/view/SearchPatient.fxml");
 
